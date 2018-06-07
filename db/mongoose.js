5e1c8261bf2cb5a953bbfa5610d6ad1731255cf8
Jeremy Bradbury <jdbradbury@gmail.com>
2018-06-07 13:37:39 -0700

    setup promises properly

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,7 +4,8 @@ module.exports = function() {
     var config = es.config;
 
     var mongoose = es.db.mongoose = require('mongoose');
-
+    mongoose.Promise = require('bluebird');
+    
     mongoose.connect(config.server.dbUri, {
         server: {
             socketOptions: {keepAlive: 300000, connectTimeoutMS: 30000}
